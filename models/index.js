5c1f058492bc45942013a5e32dddedfbee329552
Jordan Heidlage <jordanheidlage@jordans-air.lan>
2022-08-19 14:26:45 -0400

    adding homeroutes to controllers

---
+++ models/index.js
@@ -0,0 +1,13 @@
+const User = require('./User');
+const Topic = require('./Topic');
+
+User.hasMany(Project, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Project.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+module.exports = { User, Topic };
